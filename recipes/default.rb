5497861eb48495df9b9b35f21caef9c3d37bd33f
Stacey Vetzal <stacey@vetzal.com>
2018-09-04 19:23:45 -0400

    set up composer as part of build

--- recipes/default.rb
+++ recipes/default.rb
@@ -32,6 +32,16 @@ bash 'phpunit' do
   EOH
 end
 
+bash 'composer' do
+  cwd '/home/vagrant'
+  code <<-EOH
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+  php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+  php -r "unlink('composer-setup.php');"
+  EOH
+end
+
 template '/etc/nginx/sites-available/default' do
   source 'nginx-default.erb'
   owner 'root'
